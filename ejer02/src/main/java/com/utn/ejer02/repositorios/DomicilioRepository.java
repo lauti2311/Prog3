fcbc17c543c6b5f46c02d9f6aae7890e0be08dd0
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-08-29 20:00:51 -0300

    Ejercicios 1 y 2 de Programacion III

---
+++ ejer02/src/main/java/com/utn/ejer02/repositorios/DomicilioRepository.java
@@ -0,0 +1,10 @@
+package com.utn.ejer02.repositorios;
+
+import com.utn.ejer02.entidades.Domicilio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DomicilioRepository extends JpaRepository<Domicilio, Long> {
+
+}
---
+++ ejer02/src/main/java/com/utn/ejer02/repositorios/PersonaRepository.java
@@ -0,0 +1,10 @@
+package com.utn.ejer02.repositorios;
+
+import com.utn.ejer02.entidades.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository <Persona, Long> {
+
+}
