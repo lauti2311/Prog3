e28ca1ee9623957477136105eebc3a29b8c0c7d2
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-10-10 17:15:57 -0300

    Query

--- ApiRest/src/main/java/com/example/ApiRest/ApiRest/repositories/PersonaRepository.java
+++ ApiRest/src/main/java/com/example/ApiRest/ApiRest/repositories/PersonaRepository.java
@@ -2,9 +2,26 @@ package com.example.ApiRest.ApiRest.repositories;
 
 import com.example.ApiRest.ApiRest.entities.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonaRepository extends BaseRepository<Persona, Long>{
 
+    //Anotacion metodo de query
+    List<Persona> findByNombreContainingOrApellidoContaining(String nombre, String apellido);
+
+    boolean existsByDni(int dni);
+
+    //Anotaciones JPQL parametros indexados
+    @Query(value = "SELECT p FROM Persona p WHERE p.nombre LIKE '%?1%' OR p.apellido LIKE  '%?1%'")
+    List<Persona> search(String filtro);
+
+    @Query(value = "SELECT * FROM persona WHERE persona.nombre LIKE '%?1%' OR persona.apellido LIKE  '%?1%'",
+            nativeQuery = true
+
+    )
+    List<Persona> search1(String filtro);
 }
